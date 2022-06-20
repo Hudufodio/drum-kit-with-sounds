bbf0570a903f77cd6cd005a92db9c871f52e60d2
Hudu <ahmedhudu21@gmail.com>
2022-06-20 14:10:05 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+
+    const audio1 = new Audio();
+    audio1.src = "drum kits/CYCdh_SonFlam-01.wav";
+    
+
+
+
+    const audio2 = new Audio();
+    audio2.src = "drum kits/CYCdh_K1close_SnrOff-08.wav";
+
+  
+
+  
+    const audio3 = new Audio();
+    audio3.src = "drum kits/CYCdh_AcouKick-20.wav";
+ 
+
+    const audio4 = new Audio();
+    audio4.src = "drum kits/Acoustic Hat-05.wav";
+  
+  
+    const audio5 = new Audio();
+    audio5.src = "drum kits/KHats Clsd-08.wav";
+  
+
+    const audio6 = new Audio();
+    audio6.src = "drum kits/CYCdh_TrashA-07.wav";
+
+  
+
+    const audio7 = new Audio();
+    audio7.src = "drum kits/CYCdh_MultiCrashHi-01.wav";
+
+    const audio8 = new Audio();
+    audio8.src = "drum kits/KHats Clsd-08.wav";
+
+    const audio9 = new Audio();
+    audio9.src = "drum kits/CYCdh_KesKick-08.wav";
+
+
+//event listener using switch statement for different sounds.
+/*var allDivs=document.querySelectorAll(".transparent-divs").length;
+for (var i = 0; i < allDivs; i++) {
+  document.querySelectorAll(".transparent-divs")[i].addEventListener("click" function(){
+    var divHTML = this.innerHTML;
+
+    switch (divHTML) {
+      case W: 
+      var snare1 = new audio("src="drum kits\CYCdh_SonFlam-01.wav"");
+      snare1.play();
+      break;
+    }
+  })
+}*/
+
+
