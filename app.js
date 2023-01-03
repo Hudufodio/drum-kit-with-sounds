6fc50c0ccf4093c748a07eaa9173157286d1bba9
hudu <ahmedhudu21@gmail.com>
2023-01-03 16:06:34 +0100

    cleaned source code

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const audio1 = new Audio();
-audio1.src = "drum kits/CYCdh_SonFlam-01.wav";
+audio1.src = "drum audio/CYCdh_SonFlam-01.wav";
 
 const audio2 = new Audio();
-audio2.src = "drum kits/CYCdh_K1close_SnrOff-08.wav";
+audio2.src = "drum audio/CYCdh_K1close_SnrOff-08.wav";
 
 const audio3 = new Audio();
-audio3.src = "drum kits/CYCdh_AcouKick-20.wav";
+audio3.src = "drum audio/CYCdh_AcouKick-20.wav";
 
 const audio4 = new Audio();
-audio4.src = "drum kits/Acoustic Hat-05.wav";
+audio4.src = "drum audio/Acoustic Hat-05.wav";
 
 const audio5 = new Audio();
-audio5.src = "drum kits/KHats Clsd-08.wav";
+audio5.src = "drum audio/KHats Clsd-08.wav";
 
 const audio6 = new Audio();
-audio6.src = "drum kits/CYCdh_TrashA-07.wav";
+audio6.src = "drum audio/CYCdh_TrashA-07.wav";
 
 const audio7 = new Audio();
-audio7.src = "drum kits/CYCdh_MultiCrashHi-01.wav";
+audio7.src = "drum audio/CYCdh_MultiCrashHi-01.wav";
 
 const audio8 = new Audio();
-audio8.src = "drum kits/KHats Clsd-08.wav";
+audio8.src = "drum audio/KHats Clsd-08.wav";
 
 const audio9 = new Audio();
-audio9.src = "drum kits/CYCdh_KesKick-08.wav";
+audio9.src = "drum audio/CYCdh_KesKick-08.wav";
